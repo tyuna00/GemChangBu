d4d1a61e71c0337893a19eec99bbbb3d78aa8a16
qkrtuna <93079766+tyuna00@users.noreply.github.com>
2022-05-13 01:18:31 +0900

    Update MainMenuScript.cs

--- Scripts/MainMenuScript.cs
+++ Scripts/MainMenuScript.cs
@@ -6,73 +6,25 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
-
-
     void Start()
     {
-        StartCoroutine(FadeOut());
+        StartCoroutine(FadeEffect.Instance.FadeOut());
     }
 
     public void NewGame()
     {
-        StartCoroutine(FadeIn( () => SceneManager.LoadScene("TopView") ));
+        SceneDataSaver.Instance.loadNum = 0;
+        StartCoroutine(FadeEffect.Instance.FadeIn( () => SceneLoader.Instance.LoadScene("TopView") ));
     }
+
+    public void ContinueGame()
+    {
+        SceneDataSaver.Instance.loadNum = 1;
+        StartCoroutine(FadeEffect.Instance.FadeIn(() => SceneLoader.Instance.LoadScene("TopView") ));
+    }
+
     public void GameExit()
     {
         Application.Quit();
     }
-
-    #region Fade Effect
-
-    [SerializeField] GameObject FadeSet;
-    float time = 0f;
-    float F_time = 1f;
-
-    IEnumerator FadeIn(System.Action action)
-    {
-        FadeSet.SetActive(true);
-        Image image = FadeSet.GetComponent<Image>();
-
-
-        Color alpha = image.color;
-
-
-        time = 0f;
-        while (alpha.a < 1f)
-        {
-            time += (Time.deltaTime / F_time) * 2f;
-            alpha.a = Mathf.Lerp(0, 1, time);
-            image.color = alpha;
-            yield return null;
-        }
-
-        action();
-        yield return null;
-    }
-    IEnumerator FadeOut()
-    {
-        FadeSet.SetActive(true);
-        Image image = FadeSet.GetComponent<Image>();
-        Color alpha = image.color;
-
-        yield return new WaitForSeconds(0.1f);
-
-        time = 0f;
-        while (alpha.a > 0f)
-        {
-
-            time += (Time.deltaTime / F_time) * 2f;
-            alpha.a = Mathf.Lerp(1, 0, time);
-            image.color = alpha;
-            yield return null;
-        }
-
-        FadeSet.SetActive(false);
-
-        //image.color = new Color(1, 1, 1, 0);
-
-
-        yield return null;
-    }
-    #endregion
 }
